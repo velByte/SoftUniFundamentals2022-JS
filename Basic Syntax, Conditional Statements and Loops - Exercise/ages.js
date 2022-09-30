3850d79fd93f062074e22e403305374ddb58e2d1
velByte <67099502+velByte@users.noreply.github.com>
2022-09-30 23:16:23 +0200

    Updatet

--- Basic Syntax, Conditional Statements and Loops - Exercise/ages.js
+++ Basic Syntax, Conditional Statements and Loops - Exercise/ages.js
@@ -1,34 +1,17 @@
 function ages(input){
-    
-    var age = parseInt(input);
-    
-    if (age >= 0 && age <= 2) {
-        return "baby";
-    } else if (age >= 3 && age <= 13) {
-        return "child";
-    } else if (age >= 14 && age <= 19) {
-        return "teenager";
-    } else if (age >= 20 && age <= 65) {
-        return "adult";
-    } else if (age > 65) {
-        return "elder";
+    if (input >= 0 && input <= 2) {
+        console.log("baby");
+    } else if (input >= 3 && input <= 13) {
+        console.log("child");
+    } else if (input >= 14 && input <= 19) {
+        console.log("teenager");
+    } else if (input >= 20 && input <= 65) {
+        console.log("adult");
+    } else if (input >= 66) {
+        console.log("elder");
     } else {
-        return "out of bounds";
-    }
-
+        console.log("out of bounds");
+    } 
 }
 
-$el(135)
-$toJSON($get($get($el(135),$el(251)),Betrag))
 
-[{"Betrag":500,"Waehrung":{"class":"de.lobster.scm.denum.DynamicEnumDto","enumName":"de.lobster.scm.utils.units.Currency","name":"USD","ordinal":null}},{"Betrag":51643.72,"Waehrung":{"class":"de.lobster.scm.denum.DynamicEnumDto","enumName":"de.lobster.scm.utils.units.Currency","name":"EUR","ordinal":null}},{"Betrag":3250,"Waehrung":{"class":"de.lobster.scm.denum.DynamicEnumDto","enumName":"de.lobster.scm.utils.units.Currency","name":"HUF","ordinal":null}}]
-
-
-
-fa://fas-angle-right
-<p style="text-align: center; font-size: 20px; color: #A2232B;"><i class="fas fa-angle-right"></i></p>
-<p style="text-align: center; font-size: 20px; color: #A2232B;"><i class="fas fa-angle-left"></i></p>
-
-
-
-align-items: stretch;
