2bbd9f1eaa34e4637ac5b33f7278f64b3d57c977
velByte <67099502+velByte@users.noreply.github.com>
2022-11-01 17:23:54 +0100

    Added solution

---
+++ Objects and Classes - Exercise/08.ClassStorage.js
@@ -0,0 +1,39 @@
+function solve() {
+
+    class Storage {
+        constructor(capacity) {
+            this.capacity = capacity;
+            this.storage = new Array(capacity);
+            this.totalCost = 0;
+        }
+
+        addProduct(product) {
+            this.storage.push(product);
+            this.capacity -= product.quantity
+            this.totalCost += (product.price * product.quantity);
+        }
+        getProducts() {
+            let result = []
+            this.storage.forEach(item => {
+                result.push(JSON.stringify(item))
+            })
+            return result.join("\n");
+        }
+
+    }
+
+
+    let productOne = { name: 'Cucamber', price: 1.50, quantity: 15 };
+    let productTwo = { name: 'Tomato', price: 0.90, quantity: 25 };
+    let productThree = { name: 'Bread', price: 1.10, quantity: 8 };
+    let storage = new Storage(50);
+    storage.addProduct(productOne);
+    storage.addProduct(productTwo);
+    storage.addProduct(productThree);
+    console.log(storage.getProducts());
+    console.log("capacity", storage.capacity);
+    console.log(storage.totalCost);
+
+}
+
+solve();
